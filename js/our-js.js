bee39d50c469593a741aff3d9a2f59ce169db2a6
Stephanie T <srt3@sfu.ca>
2015-03-05 14:28:34 -0800

    HOLY SMOKES... Better working JS :) (Needs to be cleaned up, though!)

--- js/our-js.js
+++ js/our-js.js
@@ -8,29 +8,35 @@ $(document).ready(
 				event.preventDefault();
 
 				console.log("default prevented");
-				if ($(".graphicDesign").hasClass("gd-shown")) {
-					$(".graphicDesign").removeClass ("gd-shown").addClass("gd-hidden");
-					$(".webDesign").removeClass ("wd-hidden").addClass("wd-shown");
-					$(".photography").removeClass ("gd-hidden").addClass("gd-shown");
-					$(".interactionDesign").removeClass ("gd-hidden").addClass("gd-shown");
-					$(".illustration").removeClass ("gd-hidden").addClass("gd-shown");
+				if ($(".graphicDesign").hasClass("itsShown")) {
+					$(".graphicDesign").removeClass ("itsHidden").addClass("itsShown");
+					$(".webDesign").removeClass ("itsShown").addClass("itsHidden");
+					$(".photography").removeClass ("itsShown").addClass("itsHidden");
+					$(".interactionDesign").removeClass ("itsShown").addClass("itsHidden");
+					$(".illustration").removeClass ("itsShown").addClass("itsHidden");
 				} else {
-					$(".graphicDesign").removeClass("gd-hidden").addClass("gd-shown");
-					// $(".webDesign").removeClass("wd-shown").addClass("wd-shown");
-					// $(".interactionDesign").removeClass("gd-shown").addClass("gd-shown");
-					// $(".photography").removeClass("gd-shown").addClass("gd-shown");
-					// $(".illustration").removeClass("gd-shown").addClass("gd-shown");
+					$(".graphicDesign").removeClass("itsShown").addClass("itsHidden");
+					$(".webDesign").removeClass ("itsHidden").addClass("itsShown");
+					$(".photography").removeClass ("itsHidden").addClass("itsShown");
+					$(".interactionDesign").removeClass ("itsHidden").addClass("itsShown");
+					$(".illustration").removeClass ("itsHidden").addClass("itsShown");
 				}
-				// $(".gd-toggle").click(
-				// 		function() {
-				// 			$(".graphicDesign").show();
-				// 			console.log("function is running");
-				// 			$(".webDesign").hide();
-				// 			console.log("webDesign is hidden");
-				// 		}
-				// 	);
-
 			}
+		);
+
+		$(".all-toggle").click(
+			//there's an event happening, we can cancel
+			function(event) {
+				//when you've clicked on this link, capture the event that would have happened
+				//and cancel it (prevent it from happening)
+				event.preventDefault();
+				console.log("default prevented");
+				$(".graphicDesign").removeClass("itsHidden").addClass("itsShown");
+				$(".webDesign").removeClass ("itsHidden").addClass("itsShown");
+				$(".photography").removeClass ("itsHidden").addClass("itsShown");
+				$(".interactionDesign").removeClass ("itsHidden").addClass("itsShown");
+				$(".illustration").removeClass ("itsHidden").addClass("itsShown");
+				}
 		);
 	}
 );
